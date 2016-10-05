890979fc11a2f76ec1b0879ae56f0eb82cf8de60
sasa0 <sasa.m.milosevic@gmail.com>
2016-10-05 11:07:19 +0200

    statistics page

---
+++ app/src/main/java/milosevic/m/sasa/supermarket/ui/Statistics.java
@@ -0,0 +1,18 @@
+package milosevic.m.sasa.supermarket.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import milosevic.m.sasa.supermarket.R;
+
+/**
+ * Created by cubesschool4 on 10/5/16.
+ */
+public class Statistics extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics_page);
+    }
+}
